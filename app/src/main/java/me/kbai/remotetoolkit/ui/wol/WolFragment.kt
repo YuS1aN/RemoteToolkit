15db2cd78bf9ecfedae06d9620b12e4901458410
sean <819165594@qq.com>
2022-11-24 03:26:40 +0800

    wol func

--- app/src/main/java/me/kbai/remotetoolkit/ui/wol/WolFragment.kt
+++ app/src/main/java/me/kbai/remotetoolkit/ui/wol/WolFragment.kt
@@ -2,18 +2,70 @@ package me.kbai.remotetoolkit.ui.wol
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import me.kbai.remotetoolkit.R
 import me.kbai.remotetoolkit.base.BaseFragment
-import me.kbai.remotetoolkit.databinding.FragmentHomeBinding
 import me.kbai.remotetoolkit.databinding.FragmentWolBinding
+import me.kbai.remotetoolkit.ext.showToast
 
 class WolFragment : BaseFragment<FragmentWolBinding>() {
+    private val mViewModel by viewModels<WolViewModel>()
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ) = FragmentWolBinding.inflate(inflater)
 
-    override fun initView() {
+    override fun initView() = viewBinding.run {
+        btnSend.setOnClickListener { btn ->
+            val host = etIp.text.toString()
+            val macAddress = etMac.text.toString()
+            val portStr = etPort.text.toString()
+            val port = if (portStr.isEmpty()) 9 else portStr.toIntOrNull()
 
+            tilMac.isErrorEnabled = false
+            tilIp.isErrorEnabled = false
+            tilPort.isErrorEnabled = false
+
+            if (port == null || port < 0 || port >= 0xFFFF) {
+                tilPort.error(R.string.wol_port_error)
+                return@setOnClickListener
+            }
+
+            btn.isEnabled = false
+
+            lifecycleScope.launch {
+                mViewModel.sendWolMagicPacket(host, macAddress, port)
+                    .collect {
+                        when (it.code) {
+                            WolViewModel.CODE_SUCCESS -> {
+                                showToast(getString(R.string.wol_success, it.data))
+                            }
+                            WolViewModel.CODE_HOST_ERROR -> {
+                                tilIp.error(R.string.wol_host_error)
+                            }
+                            WolViewModel.CODE_MAC_ERROR -> {
+                                tilMac.error(R.string.wol_mac_error)
+                            }
+                            WolViewModel.CODE_SEND_ERROR -> {
+                                showToast(R.string.wol_send_error)
+                            }
+                        }
+                        btn.isEnabled = true
+                    }
+            }
+        }
     }
+
+    private fun TextInputLayout.error(@IdRes resId: Int) {
+        error = getString(resId)
+        isErrorEnabled = true
+    }
+
+
 }
--- app/src/main/java/me/kbai/remotetoolkit/ui/wol/WolViewModel.kt
+++ app/src/main/java/me/kbai/remotetoolkit/ui/wol/WolViewModel.kt
@@ -1,7 +1,56 @@
 package me.kbai.remotetoolkit.ui.wol
 
+import android.net.MacAddress
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import me.kbai.remotetoolkit.model.FuncResponse
+import java.io.IOException
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 class WolViewModel : ViewModel() {
+    companion object {
+        const val CODE_SUCCESS = 0
+        const val CODE_MAC_ERROR = 1
+        const val CODE_HOST_ERROR = 2
+        const val CODE_SEND_ERROR = 3
+    }
 
+    fun sendWolMagicPacket(host: String, mac: String, port: Int) = flow<FuncResponse<String?>> {
+        val macAddress = try {
+            MacAddress.fromString(mac)
+        } catch (e: IllegalArgumentException) {
+            emit(FuncResponse(CODE_MAC_ERROR, null, e))
+            return@flow
+        }
+        val inetAddress = try {
+            withContext(Dispatchers.IO) {
+                InetAddress.getByName(host)
+            }
+        } catch (e: IOException) {
+            emit(FuncResponse(CODE_HOST_ERROR, null, e))
+            return@flow
+        }
+        val ff = 0xFF.toUByte().toByte()
+        val macByteArray = macAddress.toByteArray()
+        val data = ByteArray(102) {
+            if (it < 6) ff else macByteArray[it % 6]
+        }
+        val packet = DatagramPacket(data, data.size, inetAddress, port)
+        try {
+            withContext(Dispatchers.IO) {
+                DatagramSocket().send(packet)
+            }
+            emit(FuncResponse(CODE_SUCCESS, inetAddress.hostAddress, null))
+        } catch (e: IOException) {
+            emit(FuncResponse(CODE_SEND_ERROR, null, e))
+        }
+    }
+
+    private fun String.decodeHex() = chunked(2)
+        .map { it.toUByte(16).toByte() }
+        .toByteArray()
 }
